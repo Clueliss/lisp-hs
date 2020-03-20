59c25f083f8c9c2045adffcca246c4e5cebd3766
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2020-03-21 00:15:17 +0100

    added alternative function syntax

--- LMLParser.hs
+++ LMLParser.hs
@@ -5,6 +5,7 @@ import Data.Char
 import qualified Data.Set as Set
 import Data.Tuple
 import Text.Read
+import Data.Function
 
 import Parser
 import LMLExpr
@@ -20,19 +21,18 @@ symbolParser = some $ parseIf $ flip elem asciiPunct
 lmlFuncDeclParser :: Parser (LMLType, [(String, LMLType)])
 lmlFuncDeclParser = (\x y -> (y, x))
     <$> (sepBy (wsP *> strP "->" <* wsP) lmlDeclParser)
-    <*> (wsP *> strP "->" *> wsP *> tp)
-    where
-        tp = ((LMLTrivialType "") <$ strP "*") <|> lmlTypeParser 
+    <*> (wsP *> strP "->" *> wsP *> lmlTypeParser)
 
 
 lmlDeclParser :: Parser (String, LMLType)
 lmlDeclParser = (,)
-        <$> identParser
-        <*> (wsP *> charP ':' *> wsP *> parseOpt lmlTypeParser (LMLTrivialType ""))
+    <$> identParser
+    <*> (wsP *> charP ':' *> wsP *> lmlTypeParser)
 
 
 lmlTypeParser :: Parser LMLType
-lmlTypeParser = genericTP 
+lmlTypeParser = (LMLTrivialType "" <$ charP '*')
+    <|> genericTP 
     <|> funTP
     <|> (LMLTrivialType <$> identParser) 
     <|> (LMLListType <$> listTP)
@@ -41,11 +41,9 @@ lmlTypeParser = genericTP
     where
         genericTP = LMLGenericType <$> (charP '\'' *> identParser)
 
-        tp = ((LMLTrivialType "") <$ strP "*") <|> lmlTypeParser
-
         funTP = (\x xs -> LMLFunctionType (last xs) (x:(init xs)))
-            <$> (charP '(' *> wsP *> tp)
-            <*> (some (wsP *> strP "->" *> wsP *> tp)) <* wsP <* charP ')'
+            <$> (charP '(' *> wsP *> lmlTypeParser)
+            <*> (some (wsP *> strP "->" *> wsP *> lmlTypeParser)) <* wsP <* charP ')'
         
         listTP  = (charP '[' *> wsP *> 
             lmlTypeParser 
@@ -105,11 +103,27 @@ lmlLambdaParser = LMLAstLambda
     <*> (wsP *> strP "->" *> wsP *> lmlParser)
 
 
+
+-- example: fun compose ( f:('b -> 'c) -> g:('a -> 'b) -> x:'a -> 'c ) = (f (g x))
 lmlFunctionParser :: Parser LMLAst
 lmlFunctionParser = (\funname (ret, args) body -> LMLAstFunction funname ret args body) 
     <$> (strP "fun" *> wsP *> (identParser <|> symbolParser) <* wsP)
     <*> (charP '(' *> wsP *> lmlFuncDeclParser <* wsP <* charP ')' <* wsP)
     <*> (charP '=' *> wsP *> lmlParser)
+
+
+-- example: fun compose f g x :: ('a -> 'b) -> ('a -> 'b) -> 'a -> 'c = (f (g x))
+lmlAltFuncParser :: Parser LMLAst
+lmlAltFuncParser = 
+    ((\name paramNames decltype body -> (name, paramNames, decltype, body))
+    <$> (strP "fun" *> wsP *> identParser)
+    <*> (wsP *> sepBy wsP identParser)
+    <*> (wsP *> strP "::" *> wsP *> sepBy (wsP *> strP "->" <* wsP) lmlTypeParser)
+    <*> (wsP *> charP '=' *> wsP *> lmlParser))
+
+    >>= (\(name, paramNames, decltype, body) -> if (length paramNames) == (length (init decltype))
+            then succeedP $ LMLAstFunction name (last decltype) (zip paramNames (init decltype)) body
+            else failP)
 
 
 ctorDeclParser :: Parser (String, [String])
